15a54c4fac17afe1f1ead3f30b1e16b777f625a2
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2023-03-11 13:40:36 +0200

    fix infinity scroll

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -9,42 +9,47 @@ import { toast } from 'react-toastify';
 function HomePage() {
   const [movies, setMovies] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [isFetching, setIsFetching] = useState(false);
 
-  const fetchMovies = useCallback(page => {
+  const fetchMovies = useCallback(() => {
+    setIsFetching(true);
+
     apiTheMovieDB
-      .fetchTrending(page)
+      .fetchTrending(currentPage)
       .then(newMovies => {
+        setIsFetching(false);
+
         if (newMovies.length === 0) {
           toast.error('sorry ,thats all the movies we cold find');
         }
+
         setMovies(prevMovies => [...prevMovies, ...newMovies]);
         setCurrentPage(prevPage => prevPage + 1);
       })
       .catch(error => {
+        setIsFetching(false);
         console.error(error);
       });
-  }, []);
-
-  function handleLoadMore() {
-    fetchMovies(currentPage);
-  }
+  }, [currentPage]);
 
   useEffect(() => {
-    fetchMovies(currentPage);
-  }, [fetchMovies, currentPage]);
+    if (movies.length === 0) {
+      fetchMovies();
+    }
+  }, [movies, fetchMovies]);
 
   return (
     <section className={css.trandingMovies}>
-      <InfiniteScroll
-        dataLength={movies.length}
-        next={handleLoadMore}
-        hasMore={true}
-        loader={<p>Loading...</p>}
-      >
-        <Container>
+      <Container>
+        <InfiniteScroll
+          dataLength={movies.length}
+          next={fetchMovies}
+          hasMore={!isFetching}
+          loader={<p>Loading...</p>}
+        >
           <MoviesList movies={movies} />
-        </Container>
-      </InfiniteScroll>
+        </InfiniteScroll>
+      </Container>
     </section>
   );
 }
